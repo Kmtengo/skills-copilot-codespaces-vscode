6ddecd4b15f9d11fb55f01dd7dd7bd4583efe38b
Kalama <97434062+Kmtengo@users.noreply.github.com>
2024-06-01 15:16:57 +0000

    Second copilot commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  // Skills object
+  this.skills = {
+    // Object with skills
+    skill1: {
+      name: 'JavaScript',
+      level: 'advanced'
+    },
+    skill2: {
+      name: 'HTML',
+      level: 'advanced'
+    },
+    skill3: {
+      name: 'CSS',
+      level: 'advanced'
+    }
+  };
+}
